ee10cd0a03730293e1ac6f6069505963b41336f9
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-02-25 15:53:59 +0200

    Usage of print and println

---
+++ Print-Printline/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+		System.out.println("I am learning Java.");
+		System.out.println("It is awesome");
+
+		System.out.print("Hello world! ");
+		System.out.print("I will print this on the same line \n");
+
+	}
+}
