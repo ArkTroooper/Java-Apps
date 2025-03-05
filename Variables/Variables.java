a0254f69700a4718f34b92319a35b52a068b2fe8
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-03-05 14:21:01 +0200

    Some variable usage

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -1,14 +1,26 @@
+import java.text.MessageFormat;
+
 public class Variables
 {
     public static void main(String[] args)
     {
-        //type variableName = value
+        // type variableName = value
         // ^ how to declare variables
+        // ^ use 'final' before type to declare a variable as constant 
 
         /*String - stores text, such as "Hello". String values are surrounded by double quotes
           int - stores integers (whole numbers), without decimals, such as 123 or -123
           float - stores floating point numbers, with decimals, such as 19.99 or -19.99
           char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
-          boolean - stores values with two states: true or false */
+          boolean - stores values with two states: true or false 
+        */
+        
+        int i = 1;
+        String s = "a string";
+        float f = 5.6f;
+        char c = 'D';
+        boolean b = true;
+
+        System.out.println(MessageFormat.format("Int: {0} \nString: {1} \nFloat: {2} \nChar: {3} \nBoolean: {4}", i,s,f,c,b));
     }
 }
