609d1d48a18de8d336ba8af21691b674674ab9f7
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-03-05 14:10:05 +0200

    basic experimentation with variables

---
+++ Variables/Variables.java
@@ -0,0 +1,14 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        //type variableName = value
+        // ^ how to declare variables
+
+        /*String - stores text, such as "Hello". String values are surrounded by double quotes
+          int - stores integers (whole numbers), without decimals, such as 123 or -123
+          float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+          char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+          boolean - stores values with two states: true or false */
+    }
+}
