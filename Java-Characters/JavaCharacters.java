469f9fc43b1451ada8f2544602bd29092b106d6f
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-03-14 09:02:28 +0200

    stuff

--- Java-Characters/JavaCharacters.java
+++ Java-Characters/JavaCharacters.java
@@ -7,8 +7,8 @@ public class JavaCharacters
         char myGrade = 'B';
         System.out.println(myGrade);
 
-        char myVar1 = 65, myVar2 = 65, myVar3 = 67; //ASCII Values
-        System.out.println(MessageFormat.format("MyVar1:{0}\nMyVar2:{1}\nMyVar3:{2}", myVar1,myVar2,myVar3));
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67; //ASCII Values
+        System.out.println(MessageFormat.format("MyVar1: {0}\nMyVar2: {1}\nMyVar3: {2}", myVar1,myVar2,myVar3));
 
         String greeting = "Hello World";
         System.out.println(greeting);
