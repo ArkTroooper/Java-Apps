4c9dd56835e9e8c852edfbc978e704532282d35a
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-03-17 13:38:05 +0200

    Retaining my code

---
+++ Operators/Operators.java
@@ -0,0 +1,28 @@
+public class Operators
+{
+    public static void main(String[] args)
+    {
+        /* 
+        Arithmetic Operators
+        
+        +   addition
+        -   subtraction
+        *   multiplication
+        /   Division
+        %   Modulus 
+        ++  increment   
+        --  decrement
+        
+        Assignment Operators
+
+        =   x=5
+        +=  x = x + 5
+        -=  x = x - 3
+        *=  x = x * 1
+        /=
+
+        */
+        int x = 100 + 50;
+
+    }
+}
