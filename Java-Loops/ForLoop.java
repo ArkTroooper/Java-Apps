0cc560f84ed033a6492a8f5e833f61b5884bcaae
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-03-31 16:38:21 +0200

    elementary stuff

--- Java-Loops/ForLoop.java
+++ Java-Loops/ForLoop.java
@@ -10,5 +10,14 @@ public class ForLoop
         {
             System.out.println(i);
         }
+        //nested loops
+        for(int i = 0;i <=10;i++)
+        {
+            System.out.println("Outer count: "+i);
+            for(int j = 0;j <=5;j++)
+            {
+                System.out.println(" inner count: "+j);
+            }
+        }
     }
 }
