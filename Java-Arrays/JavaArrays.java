de83552b68dc9813f0ffde063a80e1ac23006e7a
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-04-07 12:04:48 +0200

    Written by me, no gpt

---
+++ Java-Arrays/JavaArrays.java
@@ -0,0 +1,32 @@
+import java.text.MessageFormat;
+import java.util.Random;
+import java.lang.Object;
+
+public class JavaArrays 
+{
+    public static void main(String[] args)
+    {
+        Random r = new Random();
+        String[] cars = {"Volvo","BMW","Ford","Mazda"};
+        int[] myNums = {1,23,435,4,66,423,24423,52,16,7};
+
+        System.out.println(cars[r.nextInt(cars.length)]); //argument is exclusive for the random funtion
+        System.out.println(myNums[r.nextInt(myNums.length)]);
+
+        cars[r.nextInt(cars.length)] = "Mercedes Benz";
+        myNums[r.nextInt(myNums.length)] = 200;
+
+        System.out.println("Displaying the content of the arrays");
+
+        for (int n : myNums) 
+        {
+            System.out.println(MessageFormat.format("Number in myNums :{0}",n));
+        }
+
+        for (String c : cars) 
+        {
+            System.out.println(MessageFormat.format("Car in car :{0}",c));
+        }
+
+    }
+}
