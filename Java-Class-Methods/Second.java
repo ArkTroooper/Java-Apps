eecfbbdfa599683353560d5ac8c00a102cdb730a
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-04-25 11:25:40 +0200

    some code to keep stored

---
+++ Java-Class-Methods/Second.java
@@ -0,0 +1,9 @@
+class Second 
+{
+    public static void main(String[] args) 
+    {
+        Main myObj = new Main(); // Create an object of Main
+        myObj.fullThrotle(); // Call the fullThrotle method
+        myObj.speed(200); // Call the method with an argument
+    }
+}
