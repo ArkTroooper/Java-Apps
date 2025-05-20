466def286078df5ed3b67314c0785905b4b99e13
ArkTroooper <aaron.krishna@laysholdings.co.za>
2025-05-20 12:03:33 +0200

    all me no gpt

---
+++ Java-Fundamentals/Java-Modifiers/JavaModifiers.java
@@ -0,0 +1,24 @@
+public class Person
+{
+    private String name;
+    private int age;
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+}
+public class JavaModifiers extends Person
+{
+    public static void main(String[] args) {
+        
+    }    
+}
